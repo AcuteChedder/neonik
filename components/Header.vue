3de10d9f3a433e2ffde745e04b20a7ace11f3078
AcuteChedder <nubik8653@gmail.com>
2025-09-07 18:30:41 +0300

    0.01

---
+++ components/Header.vue
@@ -0,0 +1,53 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <header class="header">
+        <div class="header__company">
+            <h1 class="header__company-title">Neonik Company</h1>
+            <img src="/header/logo.png" alt="" class="header__company-logo">
+        </div>
+        <div class="header__nav">
+            <NuxtLink to="" class="header__nav-link">Главная</NuxtLink>
+            <NuxtLink to="" class="header__nav-link">Сотрудничество</NuxtLink>
+            <NuxtLink to="" class="header__nav-link">О нас</NuxtLink>
+            <NuxtLink to="" class="header__nav-link">Roadmap</NuxtLink>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        &__company {
+            display: flex;
+            gap: 16px;
+            color: #fff;
+            align-items: center;
+
+            &-title {
+                font-size: 16px;
+                font-family: "Inter", sans-serif;
+                font-weight: 700;
+            }
+
+            &-logo {
+                width: 39px;
+                height: 26px;
+            }
+        }
+
+        &__nav {
+            display: flex;
+            gap: 30px;
+            font-size: 14px;
+            font-weight: 400;
+            font-family: "Inter", sans-serif;
+            color: #fff;
+        }
+    }
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="layout">
+    <header>
+      <Header />
+    </header>
+
+    <main>
+      <slot />
+    </main>
+
+    <footer>
+        <Footer />
+    </footer>
+  </div>
+</template>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background-color: rgba(21, 23, 24, 1);
+}
+
+.header {
+    margin-top: 30px;
+}
+
+.layout {
+    margin-inline: 100px;
+}
+
+</style>
